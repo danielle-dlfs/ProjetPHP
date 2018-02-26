3c98c87e8f37ff819b8b08b2563ceb3eac526166
danielle-dlfs <danielledelfosse95@gmail.com>
2018-02-26 09:20:19 +0100

    correction ex C

--- 1_TP/TP_02/sem02P1c.php
+++ 1_TP/TP_02/sem02P1c.php
@@ -10,6 +10,6 @@ echo ("type de protocole utilisé : ".$_SERVER['SERVER_PROTOCOL']."</br>");
 echo ("dns du serveur : " . $_SERVER['SERVER_NAME']."</br>");
 echo ("port du serveur : ".$_SERVER['SERVER_PORT']."</br>");
 $pathParts = pathinfo('/TP/2T/tp1718_sem02.php');
-echo ("chemin permettant d'aller de la racine du serveur à votre script : </br>");
 echo '<pre>'.print_r($pathParts,1).'</pre></br>';
+echo ("chemin accès : ". $pathParts['dirname']." </br>");
 echo ("nom de votre script : ".$pathParts['basename']."</br>");
