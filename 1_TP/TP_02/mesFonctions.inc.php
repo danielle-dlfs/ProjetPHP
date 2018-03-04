853c9908816885ffee18587f2b20eb4a1d4f8421
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-04 19:18:53 +0100

    Phase 1 finie

--- 1_TP/TP_02/mesFonctions.inc.php
+++ 1_TP/TP_02/mesFonctions.inc.php
@@ -2,6 +2,56 @@
 /**
  * Created by PhpStorm.
  * User: Danielle
- * Date: 21/02/2018
- * Time: 16:10
  */
+
+function scriptInfos($param = 'new'){
+
+    switch($param) {
+        case 'new':
+           /* static $tableau = [
+                "protocol" => $_SERVER['SERVER_PROTOCOL'],
+                "port" => $_SERVER['SERVER_PORT'],
+                ];*/
+            break;
+        case 'Empty':
+            echo 'Empty';
+            break;
+        case 'Protocol':
+            echo $_SERVER['SERVER_PROTOCOL'];
+            break;
+        case 'Port':
+            echo $_SERVER['SERVER_PORT'];
+            break;
+        case 'isPortDef':
+            echo 'isPortDef';
+            break;
+        case 'dns':
+            echo 'dns';
+            break;
+        case 'path':
+            echo 'path';
+            break;
+        case 'name':
+            echo 'name';
+            break;
+        case 'short':
+            echo 'short';
+            break;
+        case 'ext':
+            echo 'extension';
+            break;
+        case 'all':
+            echo 'all';
+            break;
+        case 'full':
+            echo 'full';
+            break;
+        case 'root':
+            echo 'root';
+            break;
+    }
+}
+
+function creeTableau($uneListe, $titre, $index){
+
+}
--- 2_SITEX/phase_01/INC/request.inc.php
+++ 2_SITEX/phase_01/INC/request.inc.php
@@ -11,8 +11,14 @@ if ( count( get_included_files() ) == 1) die( '--access denied--' );
 
 function gereRequete($rq){
     switch($rq){
+        case 'sem03':
+            return "Cette fois, je te reconnais (" . $rq .")";
+        case 'sem04':
+            return "Requête «". $rq ."» : Le TP03 est disponible sur le serveur !";
         default:
-            return "Je ne connais pas ce genre de metier (" . $rq . "), aller voir à coté";
+            //return "Je ne connais pas ce genre de metier (" . $rq . "), aller voir à coté";
+            require_once ("/RES/appelAjax.php");
+            return  RES_appelAjax($rq);
     }
 }
 
