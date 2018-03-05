8e77231fd3152b5777f77af92ae769b5b0a615c6
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-05 09:14:26 +0100

    TP 04 - 1.2 mail html

---
+++ 1_TP/TP_04/mail_2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Danielle
+ * Date: 05/03/2018
+ * Time: 09:02
+ */
+
+$de = 'delfosse.da@gmail.com';
+$sujet = 'coucou';
+$entete = "From: d.delfosse@students.ephec.be \r\n";
+$entete .= "Content-Type: text/html; charset=utf-8\r\n";
+$msg = 'Bonjour <br><br> Voici un lien vers la <a href="https://193.190.65.92/HE201409/2_SITEX/phase_01/index.php">phase 1</a> du sitex';
+mail($de,$sujet, $msg, $entete);
+echo "mail envoyé";
