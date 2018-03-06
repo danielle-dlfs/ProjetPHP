f89e0cbdbf36000972c7ec1b548d4302a53d67fc
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-06 21:47:29 +0100

    Phase 02.1 - liminaire

--- 2_SITEX/phase_02/INC/request.inc.php
+++ 2_SITEX/phase_02/INC/request.inc.php
@@ -12,12 +12,14 @@ if ( count( get_included_files() ) == 1) die( '--access denied--' );
 function gereRequete($rq){
     switch($rq){
         case 'sem03':
-            return "Cette fois, je te reconnais (" . $rq .")";
+            //return "Cette fois, je te reconnais (" . $rq .")";
+            /* Phase 02 */
+            return json_encode(['display' => 'Cette fois, je te reconnais (' . $rq .')']);
         case 'sem04':
-            return "Requête «". $rq ."» : Le TP03 est disponible sur le serveur !";
+            return json_encode(['display' => 'Requête «'. $rq .'» : Le TP03 est disponible sur le serveur !']);
         default:
             //return "Je ne connais pas ce genre de metier (" . $rq . "), aller voir à coté";
             require_once ("/RES/appelAjax.php");
-            return  RES_appelAjax($rq, $action);
+            return RES_appelAjax($rq, 'action');
     }
 }
