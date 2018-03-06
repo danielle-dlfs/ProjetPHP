ccbc494dac938051cf50a1369445488f5ad02f1c
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-06 17:12:50 +0100

    TP 04 - 1.1 mail simple (version améliorée)

--- 1_TP/TP_04/mail_1.php
+++ 1_TP/TP_04/mail_1.php
@@ -6,15 +6,9 @@
  * Time: 08:38
  */
 
-// mail('d.delfosse@students.ephec.be', 'Coucou', 'Message test', 'From: delfosse.da@gmail.com');
-
-$de = 'delfosse.da@gmail.com';
-$sujet = 'coucou';
-$entete = "From: d.delfosse@students.ephec.be \r\n";
-$msg = 'Bonjour
-
-Voici mon mail "structuré".
-
-BàT.man';
-mail($de,$sujet, $msg, $entete);
-echo "mail envoyé si pas de message d'erreur";
+$pour = 'delfosse.da@gmail.com';
+$sujet = 'Mail de test';
+$msg = "Bonjour \r\n\r\nVoici mon mail \"structuré\".\r\n\r\nBàT.man";
+$de = "From: d.delfosse@students.ephec.be";
+mail($pour,$sujet,$msg,$de);
+echo 'mail envoyé si pas de message d\'erreur';
