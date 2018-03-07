c90b2cace8b9c1b965fd5b194f5ddfe5dfb4a684
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-07 15:11:54 +0100

    TP 04.3 - mail par formulaire (remaniement des variables et corrections de bug)

--- 1_TP/TP_04/P1/mail_3.php
+++ 1_TP/TP_04/P1/mail_3.php
@@ -6,9 +6,11 @@
  * Time: 09:24
  */
 
-$pour = 'd.delfosse@students.ephec.be';
+$destinataire = $_POST['destinataire'];
+$expediteur = 'd.delfosse@students.ephec.be';
 $sujet = $_POST['sujet'];
-$entete = "From:". $_POST['de'] ."\r\n";
+$entete = "From:" .$expediteur .  "\r\n";
 $entete .= "Content-Type: text/html; charset=utf-8\r\n";
 $msg = $_POST['message'];
-$ok = mail($pour,$sujet, $msg, $entete);
+
+mail($destinataire,$sujet, $msg, $entete);
