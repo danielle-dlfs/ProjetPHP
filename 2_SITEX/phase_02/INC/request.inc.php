7daf1dc9fcf18ef0b8ba5b61dc46a1ff669389b6
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-15 12:20:13 +0100

    Phase 2.5 - makeTable sans CSS

--- 2_SITEX/phase_02/INC/request.inc.php
+++ 2_SITEX/phase_02/INC/request.inc.php
@@ -9,17 +9,23 @@
 /*  Protection de fichier */
 if ( count( get_included_files() ) == 1) die( '--access denied--' );
 
+function display($txt){
+    static $toSend;
+    if (!isset($toSend['display'])) $toSend['display'] = '';
+    $toSend['display'] .= $txt;
+}
+
 function gereRequete($rq){
+    global $toSend;
     switch($rq){
         case 'sem03':
-            //return "Cette fois, je te reconnais (" . $rq .")";
-            /* Phase 02 */
-            return json_encode(['display' => 'Cette fois, je te reconnais (' . $rq .')']);
+            display("Cette fois, je te reconnais( $rq )");
+            break;
         case 'sem04':
-            return json_encode(['display' => 'Requête «'. $rq .'» : Le TP03 est disponible sur le serveur !']);
+            display("Requête « $rq » : Le TP03 est disponible sur le serveur !");
+            break;
         default:
-            //return "Je ne connais pas ce genre de metier (" . $rq . "), aller voir à coté";
-            require_once ("/RES/appelAjax.php");
-            return RES_appelAjax($rq, 'action');
+            require_once "/RES/appelAjax.php";
+            $toSend = json_decode(RES_appelAjax($rq, 'action'));
     }
 }
--- 2_SITEX/phase_02/JS/index.js
+++ 2_SITEX/phase_02/JS/index.js
@@ -1,3 +1,93 @@
+function appelAjax(elmt){
+    var request = $(elmt).attr('href').split('.html')[0];
+    $.get('index.php?rq=' + request, gereRetour);
+}
+
+function testJson(json){
+    var parsed;
+    try {
+        parsed = JSON.parse(json);
+    } catch (e) {
+        console.log('jsonError:' + json);
+    }
+    return parsed;
+}
+
+function gereRetour(retour) {
+    retour = testJson(retour);
+    for (var action in retour) {
+        switch (action) {
+            case 'display' :
+                $("#contenu").html(retour[action]).fadeIn(500);
+                break;
+            case 'error':
+                $("#error").html(retour[action]).fadeIn(500);
+                break;
+            case 'makeTable':
+                var table = makeTable(retour[action]);
+                $("#contenu").html(table).fadeIn(500);
+                break;
+            default :
+                console.log('action inconnue :' + action);
+                console.log(retour[action]);
+                break;
+        }
+    }
+}
+
+function makeTheadObject(el, type='Array'){
+    var out = '<thead>\t<tr>\n\t\t<th>' +(type =='Array' ? 'index' : 'clé') + '</th>'
+        + Object.keys(el).map(function(x){return '\t\t<th>' + x + '</th>'}).join('\n')
+        + '\t</tr>\n</thead>\n';
+    return out;
+}
+
+function makeTheadArray(el, type='Array'){
+    var out = '<thead>\t<tr>\n\t\t<th>' +(type = 'Array' ? 'index' : 'clé') + '</th>'
+       + Object.keys(el).map(function(x){return '\t\t<th>col_' + x + '</th>'}).join('\n')
+       + '\t</tr>\n</thead>\n';
+    return out;
+}
+
+function makeTbody(tab, type ='Array'){
+    var out = '<tbody>'
+       + Object.keys(tab)
+           .map(function(k){ return '\t<tr id=' + (type == 'Array' ? 'lig_' : '') + k + '>\n\t\<td>' + k + '</td>\n'
+               + Object.keys(tab[k])
+                   .map(function(x){return '\t\t<td>'+ tab[k][x] + '</td>';}).join('\n')
+               + '</tr>';
+           }).join('\n')
+       + '</tbody>';
+    return out;
+}
+
+function makeTableFromObject(tab){
+    var firstElement = tab[Object.keys(tab)[0]];
+    var elementType = firstElement.constructor.name;
+    var fonction = 'makeThead' + elementType;
+    var out = '<table class="myTab '+ elementType +'">'
+        + window[fonction](firstElement,elementType)
+        + makeTbody(tab,'Object')
+        + '</table>';
+    return out;
+}
+function makeTableFromArray(tab){
+    var firstElement = tab[Object.keys(tab)[0]];
+    var elementType = firstElement.constructor.name;
+    var fonction = 'makeThead' + elementType;
+    var out = '<table class="myTab '+ elementType +'">'
+        + window[fonction](firstElement,elementType)
+        + makeTbody(tab,'Array')
+        + '</table>';
+    return out;
+}
+
+function makeTable(tab){
+    var fonction = 'makeTableFrom' + tab.constructor.name;
+    var out = window[fonction](tab);
+    return out;
+}
+
 $(document).ready(function(){
 
     /* Au chargement de la page */
@@ -15,74 +105,29 @@ $(document).ready(function(){
     /* Rendre les liens des crédits ouvrable dans une nouvelle fenetre */
     $("#credits a").attr("target","_blank");
 
-    /* Phase 01.01 */
     $("#menu a:first").addClass('selected');
 
-    /*$(".menu a").click(appelAjax);*/
     $('.menu a').click(function(event){
-       event.preventDefault();
-       $(".menu a").removeClass('selected');
-       $(this).addClass('selected');
-       appelAjax(this);
+        event.preventDefault();
+        $(".menu a").removeClass('selected');
+        $(this).addClass('selected');
+        appelAjax(this);
     });
-    /* Phase 2 - 3.2 */
-    // $("<section id='gestion'></section>").insertAfter("#global");
-    // $("#gestion").append("<aside id='error'></aside>");
-    // $("#error").hide();
-    /* Phase 2 - 3.4 */
-   /* $("aside#error").dblclick(function(){
-        $("aside#error").fadeOut(0.5);
-    });*/
+
     /* Phase 2 - 4.2 */
     $("<section id='gestion'></section>").insertAfter("#global");
-    $("#gestion").append("<aside id='error'></aside>")
+
+    var gestion = $("#gestion");
+
+    gestion.append("<aside id='error'></aside>")
         .append("<aside id='message'></aside>")
         .append("<aside id='debug'></aside>")
         .append("<aside id='jsonError'></aside>")
         .append("<aside id='kint'></aside>");
-    // $("#gestion").children().hide();
 
-    $("#gestion").children().dblclick(function(){
-        $("#gestion").children().fadeOut(0.5);
+    gestion.children().hide();
+
+    gestion.children().dblclick(function(){
+        $(this).fadeOut(500);
     });
 });
-
-function appelAjax(elmt){
-    var request = $(elmt).attr('href').split('.html')[0];
-    $.get('index.php?rq=' + request, gereRetour);
-}
-
-function gereRetour(retour){
-    retour = testJson(retour);
-    for (var action in retour){
-        switch(action){
-            case 'display' :
-                $("main").html(retour[action]);
-                $("main").fadeIn();
-                break;
-            case 'error':
-                $("#error").html(retour[action]);
-                $("#error").fadeIn();
-               /* $("#error").click(function(){
-                    $("#error").html(retour[action]);
-                    $("#error").fadeIn();
-                });*/
-                break;
-            default :
-                console.log('action inconnue :' + action);
-                console.log(retour[action]);
-                break;
-        }
-    }
-}
-
-function testJson(json){
-    var parsed;
-    try {
-        parsed = JSON.parse(json);
-    } catch (e) {
-        console.log('jsonError:' + json);
-    }
-    return parsed;
-}
-
