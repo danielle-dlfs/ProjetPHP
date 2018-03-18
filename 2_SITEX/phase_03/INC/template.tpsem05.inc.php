f255d850550a339bec22374d0b38bc2cc9edc49e
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-19 00:46:50 +0100

    PHASE 3.1.2.1 fini

---
+++ 2_SITEX/phase_03/INC/template.tpsem05.inc.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Danielle
+ * Date: 18/03/2018
+ */
+?>
+<section id="tpsem05">
+    <fieldset id="tp05search">
+        <legend>Groupe recherché</legend>
+        <form name="recherche" id="formSearch">
+            <input type="text" id="txtSearch" name="txtSearch" placeholder="nom du groupe recherché">
+            <br>
+            <div>
+                <span title="Début (Begin)">
+                    <label for="B"><<</label>
+                    <input type="radio" id="B" value="B" name="posSearch">B</input>
+                </span>
+                <span title="In">
+                    <label for="I">  ></label>
+                    <input type="radio" id="I" value="I" name="posSearch" checked>I</input>
+                    <label for="I"><</label>
+                </span>
+                <span title="Fin (End)">
+                    <input type="radio" id="E" value="E" name="posSearch">E</input>
+                    <label for="E"> >> </label>
+                </span>
+            </div>
+        </form>
+    </fieldset>
+    <fieldset id="tp05select">
+        <legend>Suggestion</legend>
+        <form name="suges" id="formSelect" name="select">
+            <select title="Choisissez le groupe à afficher">
+            </select>
+        </form>
+    </fieldset>
+    <fieldset id="tp05result">
+        <p>Pas de groupe sélectionné</p>
+        <div></div>
+    </fieldset>
+</section>
