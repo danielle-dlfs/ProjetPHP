5df4b7e1584a45c7c22defa5ef0e9c22483287d2
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-21 18:30:35 +0100

    TP 04 - P2.3 finie

--- 1_TP/TP_04/P2/JS/up_form_3.js
+++ 1_TP/TP_04/P2/JS/up_form_3.js
@@ -1,3 +1,16 @@
 function verif(){
 
+    var extensionArray = ['jpg', 'jpeg', 'gif', 'png'];
+
+    var fileSize = document.getElementById("file").files[0].size;
+    var extFile = document.getElementById("file").value.split('.')[1].toLowerCase();
+
+    console.log(extensionArray.indexOf(extFile)); // si extension ok retourne 0
+    if (extensionArray.indexOf(extFile) < 0) {
+        document.getElementById("send").disabled = true;
+        document.getElementById("msgUpload").innerHTML = "Erreur : mauvaise extension";
+    } else {
+        document.getElementById("send").disabled = false;
+        document.getElementById("msgUpload").innerHTML = "Taille du fichier : " + fileSize + " Mb";
+    }
 }
