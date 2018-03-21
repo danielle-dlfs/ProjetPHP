4e687f35ea59a730582fdee888fa3c9b2fe1e924
danielle-dlfs <danielledelfosse95@gmail.com>
2018-03-21 18:37:47 +0100

    TP 04 - P2.3

--- 1_TP/TP_04/P2/JS/up_form_3.js
+++ 1_TP/TP_04/P2/JS/up_form_3.js
@@ -6,6 +6,7 @@ function verif(){
     var extFile = document.getElementById("file").value.split('.')[1].toLowerCase();
 
     console.log(extensionArray.indexOf(extFile)); // si extension ok retourne 0
+
     if (extensionArray.indexOf(extFile) < 0) {
         document.getElementById("send").disabled = true;
         document.getElementById("msgUpload").innerHTML = "Erreur : mauvaise extension";
