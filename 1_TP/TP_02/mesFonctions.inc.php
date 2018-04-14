731821dd6658fdb831bc51458829b52ebd7024f7
danielle-dlfs <danielledelfosse95@gmail.com>
2018-04-14 23:27:17 +0200

    Modif du TP02 - toujours pas 100£ fini

--- 1_TP/TP_02/mesFonctions.inc.php
+++ 1_TP/TP_02/mesFonctions.inc.php
@@ -5,56 +5,75 @@
  */
 
 function scriptInfos($param = 'new'){
-    $paramLowCase = strtolower($param);
+    $param = strtolower($param);
+	static $array;
 
-    switch($paramLowCase) {
+    switch($param) {
         case 'new':
-           /* static $tableau = [
-                "protocol" => $_SERVER['SERVER_PROTOCOL'],
-                "port" => $_SERVER['SERVER_PORT'],
-                ];*/
+            $url = pathinfo($_SERVER['URL']);
+			$array = array(
+				'protocol' => $_SERVER['SERVER_PORT_SECURE'] . ' - ' . isset($_SERVER['HTTPS']) ? "https" : "http",
+                'port' => $_SERVER['SERVER_PORT'],
+                'dns' => $_SERVER['SERVER_NAME'],
+                'path' => $url['dirname'],
+                'name' => $url['basename'],
+                'short' => $url['filename'],
+                'ext' => $url['extension'],
+                'isportdef' => $_SERVER['SERVER_PORT_SECURE']
+			);
             break;
+
         case 'Empty':
+			$array = null;
             echo 'Empty';
             break;
-        case 'Protocol':
-            echo $_SERVER['SERVER_PROTOCOL'];
-            break;
-        case 'Port':
-            echo $_SERVER['SERVER_PORT'];
-            break;
-        case 'isPortDef':
-            echo 'isPortDef';
-            break;
-        case 'dns':
-            echo 'dns';
-            break;
-        case 'path':
-            echo 'path';
-            break;
-        case 'name':
-            echo 'name';
-            break;
-        case 'short':
-            echo 'short';
-            break;
-        case 'ext':
-            echo 'extension';
-            break;
+
         case 'all':
             echo 'all';
+			if ($array == null || !is_array($array)) $array = scriptInfos();
             break;
+
         case 'full':
             echo 'full';
+			$array = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];            
             break;
+
         case 'root':
             echo 'root';
+			$array = $_SERVER['URL'];
+            break;
+
+		case in_array($param, array_keys(scriptInfos())):
+            $array = $aarray[$param];
+            break;
+
+		default : 
+			$array = 'Error in ' . __FUNCTION__ . ' on line ' . __LINE__ . ' : param�tre inconnu (' . $param . ')';
             break;
     }
+
+	return $array; 
 }
 
-function creeTableau($uneListe, $titre, $index){
-
+function creeTableau($uneListe, $titre = '', $index = false){
+	$out = '<table><caption>'. $titre .'</caption>';
+    $out .= '<tr>';
+    $keys = array_keys($uneListe[array_keys($uneListe)[0]]);
+    if ($index) $out .= '<th>index</th>';
+    foreach ($keys as $k) {
+        $out .= '<th>' . $k . '</th>';
+    }
+    $out .= '</tr>';
+    foreach ($uneListe as $x => $x_value) {
+        $out .= '<tr>';
+        if ($index) $out .= '<td>' . $x . '</td>';
+        foreach ($x_value as $y => $y_value) {
+            $out .= '<td>' . $y_value . '</td>';
+        }
+        $out .= '</tr>';
+    }
+    $out .= '</table>';
+    return $out;
 }
 
 function monPrint_r($liste){
--- 1_TP/TP_02/sem02P1d.php
+++ 1_TP/TP_02/sem02P1d.php
@@ -4,6 +4,73 @@
  * User: Danielle
  */
 
-require_once ("mesFonctions.inc.php");
-
-scriptInfos("new");
+require_once "mesFonctions.inc.php";
+?>
+<!DOCTYPE html>
+<table>
+            <thead>
+                <tr>
+                    <th>Paramètre</th>
+                    <th>Retour</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>new</td>
+                    <td><? print_r(scriptInfos("new"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>Empty</td>
+                    <td><? print_r(scriptInfos("empty"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>proTocol</td>
+                    <td><? print_r(scriptInfos("proTocol"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>poRt</td>
+                    <td><? print_r(scriptInfos("poRt"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>isPortDef</td>
+                    <td><? print_r(scriptInfos("isPortDef"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>dNs</td>
+                    <td><? print_r(scriptInfos("dNs"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>path</td>
+                    <td><? print_r(scriptInfos("path"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>naME</td>
+                    <td><? print_r(scriptInfos("naME"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>short</td>
+                    <td><? print_r(scriptInfos("short"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>EXT</td>
+                    <td><? print_r(scriptInfos("EXT"), true) ?></td>
+                </tr>
+                <tr>
+                    <td>all</td>
+                    <td><?= '<pre>' . print_r(scriptInfos("all"), true) . '</pre>' ?></td>
+                </tr>
+                <tr>
+                    <td>full</td>
+                    <td><?= '<pre>' . print_r(scriptInfos("full"), true) . '</pre>' ?></td>
+                </tr>
+                <tr>
+                    <td>root</td>
+                    <td><?= '<pre>' . print_r(scriptInfos("root"), true) . '</pre>' ?></td>
+                </tr>
+                <tr>
+                    <td>Machin truc</td>
+                    <td><?= '<pre>' . print_r(scriptInfos("Machin truc"), true) . '</pre>' ?></td>
+                </tr>
+            </tbody>
+        </table>
+</html>
