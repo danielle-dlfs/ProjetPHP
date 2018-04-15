6f7872fd323e45564d4a13eca54b1b4d386a9219
danielle-dlfs <danielledelfosse95@gmail.com>
2018-04-15 13:35:07 +0200

    Avancement dans TP05/Phase 3 au niveau du CSS

--- 2_SITEX/phase_03/INC/template.tpsem05.inc.php
+++ 2_SITEX/phase_03/INC/template.tpsem05.inc.php
@@ -9,29 +9,30 @@
     <fieldset id="tp05search">
         <legend>Groupe recherché</legend>
         <form name="recherche" id="formSearch">
-            <input type="text" id="txtSearch" name="txtSearch" placeholder="nom du groupe recherché">
-            <br>
+            <input type="text" id="txtSearch" name="txtSearch" class="I" placeholder="nom du groupe recherché">
             <div>
                 <span title="Début (Begin)">
-                    <label for="B"><<</label>
+                    <label for="B">&#8676;</label>
                     <input type="radio" id="B" value="B" name="posSearch">B</input>
                 </span>
                 <span title="In">
-                    <label for="I">  ></label>
+                    <label for="I">&#8676;</label>
                     <input type="radio" id="I" value="I" name="posSearch" checked>I</input>
-                    <label for="I"><</label>
+                    <label for="I">&#8677;</label>
                 </span>
                 <span title="Fin (End)">
                     <input type="radio" id="E" value="E" name="posSearch">E</input>
-                    <label for="E"> >> </label>
+                    <label for="E">&#8677;</label>
                 </span>
             </div>
         </form>
     </fieldset>
     <fieldset id="tp05select">
         <legend>Suggestion</legend>
-        <form name="suges" id="formSelect" name="select">
+        <form name="suges" id="formSelect" name="select" size="10">
             <select title="Choisissez le groupe à afficher">
+                <option value="">a</option>
+                <option value="" class="deja">b</option>
             </select>
         </form>
     </fieldset>
@@ -41,3 +42,54 @@
         <div></div>
     </fieldset>
 </section>
+<style>
+    #tpsem05 {
+        display : flex;
+        flex-direction : row;
+        flex-flow : row wrap;
+    }
+
+    #formSearch {
+        text-align : center;
+    }
+
+    #formSearch label {
+        margin: 0;
+        font-size: 1.5em
+    }
+
+    #txtSearch {
+        height: 2em;
+        width: 12em;
+        font-size: 1.25em;
+    }
+
+    #txtSearch.E {
+        text-align: right;
+    }
+
+    #txtSearch.I {
+        text-align: center;
+    }
+
+    #txtSearch.B {
+        text-align: left;
+    }
+
+    #formSearch span {
+        margin: 0.25em;
+        padding: 0.25em;
+        box-shadow: 0.05em 0.05em darkgrey;
+        border-radius: 1em;
+        background-color: rgba(255, 255, 255, 0.67);
+    }
+
+    #formSelect {
+        width: 10em;
+        overflow-y: scroll;
+    }
+
+    #formSelect option.deja::before {
+        content:'\21AA \00a0';
+    }
+</style>
