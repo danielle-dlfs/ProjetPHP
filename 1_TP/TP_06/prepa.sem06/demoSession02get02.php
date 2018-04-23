87a42caa7129a0b6c80cb4ac88613dd7398b175d
danielle-dlfs <danielledelfosse95@gmail.com>
2018-04-23 13:44:16 +0200

    correction de l'exemple

--- 1_TP/TP_06/prepa.sem06/demoSession02get02.php
+++ 1_TP/TP_06/prepa.sem06/demoSession02get02.php
@@ -19,10 +19,7 @@ session_start();
 <body>
 
 <?php
-// Echo session variables that were set on previous page
-echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
-echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+print_r($_SESSION);
 ?>
-
 </body>
 </html>
