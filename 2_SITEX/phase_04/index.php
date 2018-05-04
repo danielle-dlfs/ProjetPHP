14f30ab8b5216365f4a44f17b95b50f838ed9c49
danielle-dlfs <danielledelfosse95@gmail.com>
2018-05-04 13:09:53 +0200

    PHASE 4 - jusque 2.1

--- 2_SITEX/phase_04/index.php
+++ 2_SITEX/phase_04/index.php
@@ -5,30 +5,36 @@
  */
 
 session_start();
-if(!isset($_SESSION['start'])) $_SESSION['start'] = date('amjhms');
-$_SESSION['log'] = [];
-
-if(isset($_GET['rq'])){
-    $toSend = [];
-
-    require_once "/ALL/kint/kint.php";
-    Kint::$return = true;
-    // imposer aux méthodes de la class Kint d'envoyer leur résultat par un return à la place d'un echo.
-    require_once "INC/request.inc.php";
-    gereRequete($_GET['rq']);
-    die(json_encode($toSend));
+if(!isset($_SESSION['start'])) {
+    $_SESSION['start'] = date('YmdHis');
+    $_SESSION['log'] = [];
 }
 
 require_once "INC/dbConnect.inc.php";
 require_once "INC/mesFonctions.inc.php";
+require_once "/ALL/kint/kint.php";
+Kint::$return = true;
+
+if(isset($_GET['rq'])){
+    $_SESSION['log'][time()] = $_GET['rq'];
+    $toSend = [];
+    // imposer aux méthodes de la class Kint d'envoyer leur résultat par un return à la place d'un echo.
+    require_once "INC/request.inc.php";
+    gereRequete($_GET['rq']);
+    die(json_encode($toSend));
+} else {
+    $_SESSION['log'][time()] = 'reset F5';
+}
 
 $home = 'Accueil';
 $siteName = 'SITEX : phase 04';
 $logoPath = 'IMG/04.png';
 $logoAlt = 'Logo';
-$mainZone = 'Bienvenue';
-$mainZone .= ' kint est la';
+
+$mainZone = 'Bienvenue kint est la';
+$mainZone .= d($_SESSION);
 
 $mail = ___MATRICULE___ . '@students.ephec.be';
 $author = '<a href="mailto:' . $mail . '" title="'. $mail . '">' . $__INFOS__['nom'] . ' ' . $__INFOS__['prenom']. '</a>';
+
 require_once "INC/layout.html.inc.php";
