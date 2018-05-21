280bca94514329afbe0bc7b263de06eca9dc6a76
danielle-dlfs <danielledelfosse95@gmail.com>
2018-05-21 18:11:28 +0200

    TP 11 - P1 FINIE

--- 1_TP/TP_11/tp05/JS/index.js
+++ 1_TP/TP_11/tp05/JS/index.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
-    $.ajax('charge.php').done(traitement);
+
+    var data = new FormData();
+    data.append('senderId', 'formTP05');
+    data.append('select', '2T');
+    $.ajax({
+        url: "/he201409/2_SITEX/PHASE_03/index.php?rq=formSubmit",
+        processData: false,
+        contentType: false,
+        method: 'POST',
+        data: data
+    }).done(traitement)
+
 });
 
-function traitement(json){
-    var data = JSON.parse(json).data; // on mémorise le contenu de l'item data
+function traitement(json) {
+    var data = JSON.parse(json).makeTable;
     var keys = Object.keys(data[0]);
-    var columns = keys.map(function (x){ return {title: x} });
-    var dataSet = data.map(function (x) { return keys.map(function (xk) { return x[xk] }) });
-    $("body").html("<table id='example' class='display' style='width:100%'></table>");
-    $("#example").dataTable({
-        data : dataSet,
-        columns : columns,
+    var columns = keys.map(function (x) { return {title: x} });
+    var dataset = data.map(function (x) { return keys.map(function (xk) { return x[xk] }) });
+    $('body').html('<table id="example" class="display" style="width: 100%"></table>');
+    $('#example').addClass('nowrap').dataTable({
+        data: dataset,
+        columns: columns,
         responsive: true,
-        language: {url : "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"},
-        lengthMenu: [[5, 10, 25, 50, -1], ['cinq', 10, 25, 50, 'toutes']],
-        columnDefs: [{ targets: [-1, -3], className: 'dt-body-right' }]
-        // pageLength: 10
+        language: { url: "https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json" },
+        lengthMenu: [[5, 10, 25, 50, -1], ['cinq', 10, 25, 50, 'toutes']]
     });
 }
